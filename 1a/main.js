e1e68be93be414750de6d09034d9cb0505ea8d0c
zakariyamahamud10-gmail <zakariyamahamud10@gmail.com>
2026-01-20 16:04:46 +0100

    first commit

---
+++ 1a/main.js
@@ -0,0 +1,18 @@
+import divide, { add, sub, mul, PI, SECRET_SAUCE } from './math.js';
+console.log(add(5, 3));       // 8
+console.log(divide(20, 4));   // 5
+console.log(mul(2, 8))
+console.log(divide(8, 2))
+
+console.log(SECRET_SAUCE)
+console.log(`PI är ungefär ${PI}`);
+
+
+import { add as addition } from "./math.js"
+
+
+import { PI as pivalue} from './constants.js';
+
+import * as MathUtils from "./math.js"
+console.log(MathUtils.add(2,3))
+console.log(MathUtils.mul(2, 30))
---
+++ 1a/math.js
@@ -0,0 +1,20 @@
+export function add(a,b) {
+    return a + b;
+}
+
+export function sub(a,b) {
+    return a - b
+}
+
+export function mul(a,b) {
+    return a * b
+}
+
+
+export default function divide(a, b) {
+  return a / b;
+}
+
+export const PI = 3.14159;
+export const SECRET_SAUCE = 42;
+
